44973a57291bd5d3ace0f916b0209b457614226d
kbqqq <1074408581@qq.com>
2018-10-09 23:15:37 +0800

    rename test3

--- git-update.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-if [ ! -n "$1" ]; then
-	echo "usage: git update [Reason To Update]"
-	echo "You need to add a pair of quotations if your reason contains spaces."
-else
-	git add . && git commit -m "$1" && git push -u origin master
-fi
